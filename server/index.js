915c51e57f5a6099c9c5efe46ad892c7a8929467
Paul Rogers <welshwebdeb88@gmail.com>
2021-04-22 20:37:57 +0100

    Backend files set up with server and mongoDB connection

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+//Set up server
+const app = express();
+const port = process.env.PORT || 3001;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+//Database
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('MongoDB database connection has been established successfully!');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
